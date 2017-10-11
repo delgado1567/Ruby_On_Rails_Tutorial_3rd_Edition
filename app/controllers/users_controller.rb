e3125e8fbc5b773474e90405393b868dfc51ad47
Bryon D. Foltz <bryon@bryonfoltz.com>
2017-10-10 21:04:51 -0400

    Handle edit User errors + fix syntax error in Users controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,15 @@ class UsersController < ApplicationController
 	end
 	def edit
 		@user = User.find(params[:id])
+	end
+	def update
+		@user = User.find(params[:id])
+		if @user.update_attributes(user_params)
+			#
+		else
+				render 'edit'
+		end
+	end
 
 	private
 		def user_params
