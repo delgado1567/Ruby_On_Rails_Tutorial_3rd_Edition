7e0ae1e4c7acdfbe698b73924ef8c3cbe47da55e
Bryon D. Foltz <bryon@bryonfoltz.com>
2017-10-28 20:51:38 -0400

    Update User signup integration test to include account activation.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 	def create
 		@user = User.find_by(email: params[:session][:email].downcase)
 		if @user && @user.authenticate(params[:session][:password])
-			if user.activated?
+			if @user.activated?
 				log_in(@user)
 				params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
 				redirect_back_or @user
@@ -13,6 +13,7 @@ class SessionsController < ApplicationController
 				message = "Check you remail for the activation link."
 				flash[:warning] = message
 				redirect_to root_url
+			end
 		else
 			flash.now[:danger] = 'Invalid email/password combination'
 			render 'new'
