c1893da8bdba546c5a27319e77963ec2f78d72da
Bryon D. Foltz <bryon@bryonfoltz.com>
2017-10-30 18:08:31 -0400

    Add tests for Microposts.

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -16,7 +16,7 @@ class MicropostsController < ApplicationController
 	def destroy
 		@micropost.destroy
 		flash[:success] = "Micropost deleted"
-		redirect_to request.referrer || root.url
+		redirect_to request.referrer || root_url
 	end
 
 	private
