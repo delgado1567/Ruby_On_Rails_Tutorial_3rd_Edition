86e6c4b082f8e6c9308bcfa96e859a121e649841
Bryon D. Foltz <bryon@bryonfoltz.com>
2017-10-31 20:02:53 -0400

    Add followed following statistics to Home page.

--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,11 @@ Rails.application.routes.draw do
 	post 'login' => 'sessions#create'
 
 	delete 'logout' => 'sessions#destroy'
+	resources :users do
+		member do
+			get :following, :followers
+		end
+	end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
