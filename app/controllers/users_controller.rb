b5fce7c5b767443210178afac0eb031ca62fb8fe
Bryon D. Foltz <bryon@bryonfoltz.com>
2017-10-31 20:34:26 -0400

    Add folloing followers views.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,11 +42,12 @@ class UsersController < ApplicationController
 	end
 	def following
 		@title = "Following"
-		@user = User.find(params([:id]))
+		@user = User.find(params[:id])
 		@users = @user.following.paginate(page: params[:page])
 		render 'show_follow'
 	end
 	def followers
+		@title = "Followers"
 		@user = User.find(params[:id])
 		@users = @user.followers.paginate(page: params[:page])
 		render 'show_follow'
