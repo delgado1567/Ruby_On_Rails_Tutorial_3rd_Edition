c1d221e52071b11cd6438f1468efe140d4f94d0b
Bryon D. Foltz <bryon@bryonfoltz.com>
2017-11-02 14:38:02 -0400

    Add test for Relationship statistics.

--- test/integration/users_show_test.rb
+++ test/integration/users_show_test.rb
@@ -20,6 +20,10 @@ class UsersShowTest < ActionDispatch::IntegrationTest
 		assert_select 'h1>img.gravatar'
 		assert_match @admin.microposts.count.to_s, response.body
 		assert_select 'div.pagination'
+		assert_match "#{@admin.following.count}", response.body
+		assert_match "#{@admin.followers.count}", response.body
+		assert_select "a[href=?]", following_user_path(@admin)
+		assert_select "a[href=?]", followers_user_path(@admin)
 		@admin.microposts.paginate(page:1).each do |micropost|
 			assert_match micropost.content, response.body
 		end
